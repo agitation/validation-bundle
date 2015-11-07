151fa9e4bc643b4969de51ddd6ca4caaca883cec
Alex Günsche <mail@lxg.de>
2015-11-07 22:42:32 +0100

    inherit from other trait, remove duplicate code

--- Entity/GeneratedIdentityAwareTrait.php
+++ Entity/GeneratedIdentityAwareTrait.php
@@ -11,47 +11,12 @@ namespace Agit\CoreBundle\Entity;
 
 trait GeneratedIdentityAwareTrait
 {
+    use IdentityAwareTrait;
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
     private $id;
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * This is mostly useful for comparison and sorting.
-     */
-    public function __toString()
-    {
-        return get_class() . '-' . (string)$this->getId();
-    }
-
-    public function getEntityClass()
-    {
-        $className = get_class($this);
-
-        if (strpos($className, 'Prox') !== false)
-            $className = get_parent_class($this);
-
-        if (strpos($className, '\\') !== false)
-            $className = substr(strrchr($className, "\\"), 1);
-
-        return $className;
-    }
-
-    // can be overridden with something that returns the natural, localized entity name.
-    public function getEntityName()
-    {
-        return $this->getEntityClass();
-    }
 }
