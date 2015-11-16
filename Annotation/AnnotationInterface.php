877e2f4f1f5da7a38a8dfd3cec003af54112cf2f
Alex Günsche <mail@lxg.de>
2015-11-16 13:50:05 +0100

    we no longer need the AbstractAnnotation class

---
+++ Annotation/AnnotationInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package    agitation/common
+ * @link       http://github.com/agitation/AgitCommonBundle
+ * @author     Alex Günsche <http://www.agitsol.com/>
+ * @copyright  2012-2015 AGITsol GmbH
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\CommonBundle\Annotation;
+
+use Agit\CommonBundle\Exception\InternalErrorException;
+
+interface AnnotationInterface
+{
+    public function setOptions(array $options = null);
+
+    public function getOptions();
+
+    public function set($key, $value);
+
+    public function get($key);
+}
---
+++ Annotation/SerializableAnnotationInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package    agitation/common
+ * @link       http://github.com/agitation/AgitCommonBundle
+ * @author     Alex Günsche <http://www.agitsol.com/>
+ * @copyright  2012-2015 AGITsol GmbH
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\CommonBundle\Annotation;
+
+interface SerializableAnnotationInterface extends \Serializable
+{
+}
