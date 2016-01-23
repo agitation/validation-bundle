9ae7fdc22f44ab0f78f3ff4850ca0db304512cc0
Alex Günsche <mail@lxg.de>
2016-01-23 20:09:32 +0100

    add method for setting and providing service instances

--- Resources/public/js/var/init.js
+++ Resources/public/js/var/init.js
@@ -1,19 +1,32 @@
-window.agit =
-{
-    // this method will be used at the top of each namespaced file to create the namespace on the fly
-    ns : function(namespace)
+(function(){
+    var services = {};
+
+    window.agit =
     {
-        var
-            parts = namespace.split("."),
-            i = 0,
-            ref = window;
-
-        for (i; i < parts.length; i++)
+        // this method will be used at the top of each namespaced file to create the namespace on the fly
+        ns : function(namespace)
         {
-            ref[parts[i]] = ref[parts[i]] || {};
-            ref = ref[parts[i]];
-        }
-    },
+            var
+                parts = namespace.split("."),
+                i = 0,
+                ref = window;
 
-    cfg : {}
-}
+            for (i; i < parts.length; i++)
+            {
+                ref[parts[i]] = ref[parts[i]] || {};
+                ref = ref[parts[i]];
+            }
+        },
+
+        cfg : {},
+
+        srv : function(name, instance)
+        {
+            if (instance)
+                services[name] = instance;
+            else
+                return services[name];
+        }
+    };
+})();
+
