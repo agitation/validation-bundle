1b9bf14f8072f03168955e90f509c0977566294f
Alex Günsche <mail@lxg.de>
2016-01-23 11:48:58 +0100

    initialized the agit namespace and config

---
+++ Resources/public/js/var/init.js
@@ -0,0 +1,19 @@
+window.agit =
+{
+    // this method will be used at the top of each namespaced file to create the namespace on the fly
+    ns : function(namespace)
+    {
+        var
+            parts = namespace.split("."),
+            i = 0,
+            ref = window;
+
+        for (i; i < parts.length; i++)
+        {
+            ref[parts[i]] = ref[parts[i]] || {};
+            ref = ref[parts[i]];
+        }
+    },
+
+    cfg : {}
+}
