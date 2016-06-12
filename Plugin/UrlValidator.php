c094f7ec7e9d43d5fef37284800c944b11e043cb
Alexander Günsche <mail@lxg.de>
2016-06-12 13:01:31 +0200

    improve error message

--- Plugin/UrlValidator.php
+++ Plugin/UrlValidator.php
@@ -18,17 +18,17 @@ use Agit\IntlBundle\Translate;
  */
 class UrlValidator extends AbstractValidator
 {
-    public function validate($value, $strict=true)
+    public function validate($value, $strict = true)
     {
         if (!filter_var($value, FILTER_VALIDATE_URL) || strpos($value, 'http') !== 0)
-            throw new InvalidValueException(Translate::t("The website is invalid."));
+            throw new InvalidValueException(Translate::t("The URL is invalid."));
 
         $urlParts = parse_url($value);
 
         if (!$urlParts || !isset($urlParts['scheme']) || !isset($urlParts['host']) || !in_array($urlParts['scheme'], array('http', 'https')) || !preg_match("#^(([a-z]|[a-z][a-z0-9\-]*[a-z0-9])\.)*([a-z]|[a-z][a-z0-9\-]*[a-z0-9])$#i", $urlParts['host']))
-            throw new InvalidValueException(Translate::t("The website is invalid."));
+            throw new InvalidValueException(Translate::t("The URL is invalid."));
 
         if ($strict && (isset($urlParts['user']) || isset($urlParts['pass']) || isset($urlParts['port'])))
-            throw new InvalidValueException(Translate::t("The website is malformed."));
+            throw new InvalidValueException(Translate::t("The URL is malformed."));
     }
 }
