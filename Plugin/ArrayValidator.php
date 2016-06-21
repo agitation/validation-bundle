cc46bba7bce08ee83d879a4881428b586a36de9c
Alexander Günsche <mail@lxg.de>
2016-06-21 15:00:17 +0200

    allow defining a fixed set of array keys

--- Plugin/ArrayValidator.php
+++ Plugin/ArrayValidator.php
@@ -18,15 +18,27 @@ use Agit\IntlBundle\Translate;
  */
 class ArrayValidator extends AbstractValidator
 {
-    public function validate($value, $minLength = null, $maxLength = null)
+    public function validate($value, $minLength = null, $maxLength = null, array $requireKeys = null, $allowOtherKeys = false)
     {
         if (!is_array($value))
             throw new InvalidValueException(Translate::t("The value must be an array."));
 
         if (is_int($minLength) && count($value) < $minLength)
-            throw new InvalidValueException(sprintf(Translate::t("The list is too short, it must have at least %s elements."), $minLength));
+            throw new InvalidValueException(sprintf(Translate::t("The array is too short, it must have at least %s elements."), $minLength));
 
         if (is_int($maxLength) && count($value) > $maxLength)
-            throw new InvalidValueException(sprintf(Translate::t("The list is too long, it must have at most %s elements."), $maxLength));
+            throw new InvalidValueException(sprintf(Translate::t("The array is too long, it must have at most %s elements."), $maxLength));
+
+        if (!is_null($requireKeys))
+        {
+            $keys = array_keys($value);
+
+            if ($allowOtherKeys && count($keys) !== count($requireKeys))
+                throw new InvalidValueException(Translate::t("The array has invalid keys."));
+
+            foreach ($requireKeys as $key)
+                if (!in_array($key, $keys))
+                    throw new InvalidValueException(sprintf(Translate::t("The array is missing the mandatory `%s` key."), $key));
+        }
     }
 }
