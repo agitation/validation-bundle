f255096bf378b08ee365b033eaa0f0899ea96f60
Alexander Günsche <mail@lxg.de>
2016-07-25 22:00:07 +0200

    add trait for entity name fields

---
+++ Entity/NameAwareTrait.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package    agitation/common
+ * @link       http://github.com/agitation/AgitCommonBundle
+ * @author     Alex Günsche <http://www.agitsol.com/>
+ * @copyright  2012-2015 AGITsol GmbH
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\CommonBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+trait NameAwareTrait
+{
+    /**
+     * @ORM\Column(type="string", length=70)
+     * @Assert\NotBlank
+     */
+    private $name;
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
