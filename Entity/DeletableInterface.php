150b9e48a737d2083c4988a8e4be71a511e4beea
Alexander Günsche <mail@lxg.de>
2016-07-25 21:36:44 +0200

    introduce soft-delete entity feature

---
+++ Entity/DeletableInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package    agitation/common
+ * @link       http://github.com/agitation/AgitCommonBundle
+ * @author     Alex Günsche <http://www.agitsol.com/>
+ * @copyright  2012-2015 AGITsol GmbH
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\CommonBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Indicates that an entity can be soft-deleted.
+ * Should be used together with the DeletableTrait.
+ */
+interface DeletableInterface
+{
+    public function setDeleted($deleted);
+
+    public function isDeleted();
+}
---
+++ Entity/DeletableTrait.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package    agitation/common
+ * @link       http://github.com/agitation/AgitCommonBundle
+ * @author     Alex Günsche <http://www.agitsol.com/>
+ * @copyright  2012-2015 AGITsol GmbH
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\CommonBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Provides the capability for soft-deleting entities.
+ * Must be used together with the DeletableInterface to make sense.
+ */
+trait DeletableTrait
+{
+    /**
+     * @ORM\Column(type="boolean")
+     * @Assert\NotNull
+     */
+    protected $deleted = false;
+
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+    }
+
+    /**
+     * Get deleted
+     *
+     * @return smallint
+     */
+    public function isDeleted()
+    {
+        return $this->deleted;
+    }
+}
