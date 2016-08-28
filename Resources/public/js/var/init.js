bb59c5af53c03b6492afb98435d3e929c10cbc16
Alexander Günsche <mail@lxg.de>
2016-08-28 12:43:48 +0200

    support preset for window.ag

--- Resources/public/js/var/init.js
+++ Resources/public/js/var/init.js
@@ -1,31 +1,28 @@
 (function(){
     var services = {};
 
-    window.ag =
+    window.ag = window.ag || {};
+
+    // this method will be used at the top of each namespaced file to create the namespace on the fly
+    ag.ns = function(namespace)
     {
-        // this method will be used at the top of each namespaced file to create the namespace on the fly
-        ns : function(namespace)
+        var
+            parts = namespace.split("."),
+            i = 0,
+            ref = window;
+
+        for (i; i < parts.length; i++)
         {
-            var
-                parts = namespace.split("."),
-                i = 0,
-                ref = window;
-
-            for (i; i < parts.length; i++)
-            {
-                ref[parts[i]] = ref[parts[i]] || {};
-                ref = ref[parts[i]];
-            }
-        },
-
-        cfg : {},
-
-        srv : function(name, instance)
-        {
-            if (instance)
-                services[name] = instance;
-            else
-                return services[name];
+            ref[parts[i]] = ref[parts[i]] || {};
+            ref = ref[parts[i]];
         }
+    };
+
+    ag.srv = function(name, instance)
+    {
+        if (instance)
+            services[name] = instance;
+        else
+            return services[name];
     };
 })();
